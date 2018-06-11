348f981ceb34445ff0765c9d93cedad4b1d4e6b6
vmunson <virginia.munson@gmail.com>
2018-06-11 13:56:06 -0400

    classes, objects, properties

---
+++ classes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Donut myDonut = new Donut();
+            Donut yourDonut = new Donut();
+            Donut herDonut = new Donut();
+
+            myDonut.Type = "Jelly Filled";
+            myDonut.Filling = "Strawberry Jelly";
+
+            yourDonut.Price = 2.50m;
+
+            herDonut.isSpecial = false;
+
+            Console.WriteLine($"{myDonut.Type} {yourDonut.Price} {herDonut.isSpecial}");
+        }
+    }
+}
---
+++ classes/donut.cs
@@ -0,0 +1,7 @@
+public class Donut
+    {
+        public string Type { get; set; }
+        public string Filling { get; set; }
+        public decimal Price { get; set; }
+        public bool isSpecial { get; set; }
+    }
