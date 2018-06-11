f3d0b4af1c28853ff8052a70c2df4e6a8ad215a5
vmunson <virginia.munson@gmail.com>
2018-06-11 11:46:24 -0400

    added numbers

--- variables/Program.cs
+++ variables/Program.cs
@@ -10,6 +10,21 @@ namespace variables
             var myAge = 27;
             Console.WriteLine($"Hello, my name is {myName}. I am {myAge} years old.");
             Console.WriteLine($"{myName.Length}");
+
+            string sentence = "Today is Monday";
+            Console.WriteLine(sentence);
+            sentence = sentence.Replace("Monday", "Thursday");
+            Console.WriteLine(sentence);
+
+            int integer = 42;
+            float number = 1.33f;
+            double bigNumber = 3.1131313d;
+            decimal biggestNumber = 4.131313144444444m;
+            int otherInteger = 22;
+            Console.WriteLine(integer - otherInteger * integer / otherInteger);
+            Console.WriteLine($"The numba rumba {integer}, {number}, {bigNumber}, {biggestNumber}");
+
+            bool isTrue = true;
         }
     }
 }
