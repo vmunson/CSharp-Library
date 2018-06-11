7cb55e1837fc946f8a0bc3bee43f33fca3b3b67c
vmunson <virginia.munson@gmail.com>
2018-06-11 13:25:07 -0400

    loops are fun

---
+++ loops/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 100;
+
+            while (num > 0)
+            {
+                Console.WriteLine($"{num} is greater than 0");
+                num -= 10;
+            }
+
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine($"{i} is less than 11");
+            }
+        }
+    }
+}
