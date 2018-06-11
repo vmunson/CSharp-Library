c15d0a2069557bbd78aa0d4bb254da8f5860b4f3
vmunson <virginia.munson@gmail.com>
2018-06-11 11:16:30 -0400

    learning c#

---
+++ variables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myName = "Ginny";
+            var myAge = 27;
+            Console.WriteLine($"Hello, my name is {myName}. I am {myAge} years old.");
+            Console.WriteLine($"{myName.Length}");
+        }
+    }
+}
